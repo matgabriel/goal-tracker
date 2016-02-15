70e735dcc1bb69ba50780e45abc5d41948ea0f64
Christophe Porteneuve <tdd@tddsworld.com>
2016-02-15 15:31:17 +0100

    Début étape 17 : reset quotidien

---
+++ src/reducers/closeDay.js
@@ -0,0 +1,18 @@
+// Types d’actions
+// ---------------
+
+const CLOSE_DAY = 'goal-tracker/closeDay/CLOSE_DAY'
+
+// Réducteur
+// ---------
+
+export default function reduceCloseDay(state, action) {
+  // Votre code ici…
+}
+
+// Action Creators
+// ---------------
+
+export function closeDay() {
+  return { type: CLOSE_DAY }
+}
