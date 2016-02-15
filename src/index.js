25543d19069bb52703845d82dcbd13d884a7d604
Christophe Porteneuve <tdd@tddsworld.com>
2016-02-15 16:09:11 +0100

    Étape 19 : Offline-first avec SW et AppCache

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import { install as installOfflineHandling } from 'offline-plugin/runtime'
 import React from 'react'
 import { render } from 'react-dom'
 
 import App from './App'
 
 import './lib/clock'
+
+installOfflineHandling()
 
 render(<App />, document.getElementById('root'))
 
