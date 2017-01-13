cb9138496c8b20f32a2d29d638efba51e6375c31
Christophe Porteneuve <tdd@tddsworld.com>
2017-01-13 15:46:42 +0100

    Étape 1 : Bases HomeScreen et TrackerScreen

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -1,16 +1,53 @@
 import { hot } from 'react-hot-loader/root'
 import React from 'react'
 
-// import ArrowForward from '@material-ui/icons/ArrowForward'
-// import Button from '@material-ui/core/Button'
-// import Card from '@material-ui/core/Card'
-// import CardActions from '@material-ui/core/CardActions'
-// import CardContent from '@material-ui/core/CardContent'
-// import CardHeader from '@material-ui/core/CardHeader'
-// import TextField from '@material-ui/core/TextField'
+import ArrowForward from '@material-ui/icons/ArrowForward'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
+import TextField from '@material-ui/core/TextField'
+
+import TogglablePasswordField from './TogglablePasswordField'
+
+import './LoginScreen.styl'
 
 const LoginScreen = () => {
-  return <p>COMING SOON: LOGIN SCREEN</p>
+  return (
+    <form>
+      <Card className='loginScreen'>
+        <CardHeader title='Goal Tracker' subheader='Connexion' />
+        <CardContent>
+          <TextField
+            autoComplete='home email'
+            id='email'
+            label='E-mail'
+            fullWidth
+            margin='normal'
+            placeholder='mon@email.tld'
+            required
+            type='email'
+          />
+          <TogglablePasswordField
+            autoComplete='current-password'
+            id='password'
+            label='Mot de passe'
+            fullWidth
+            margin='normal'
+            placeholder='super mot de passe'
+            required
+          />
+        </CardContent>
+        <CardActions style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button color='primary' type='submit' variant='contained'>
+            <ArrowForward />
+            Connecte-toi
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+  )
 }
 
 export default hot(LoginScreen)
