c3ce90f5bb28c597cfcc4b311d33a9c3ba3a77a8
Christophe Porteneuve <tdd@tddsworld.com>
2017-01-21 16:39:35 +0100

    Étape 17 : reset quotidien

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import reduceReducers from 'reduce-reducers'
 
 import closeDay from './closeDay'
 import currentUser from './currentUser'
@@ -7,12 +8,14 @@ import history from './history'
 import today from './today'
 import todaysProgress from './todaysProgress'
 
-const goalTrackerReducer = combineReducers({
+const coreReducer = combineReducers({
   currentUser,
   goals,
   history,
   today,
   todaysProgress,
 })
+
+const goalTrackerReducer = reduceReducers(coreReducer, closeDay)
 
 export default goalTrackerReducer
