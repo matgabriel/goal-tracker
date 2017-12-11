8e21facc4f76d01bbed672f5542cbe6e5c538f0b
Christophe Porteneuve <tdd@tddsworld.com>
2017-12-11 20:59:52 +0100

    Étape 11 : montée des objectifs dans TrackerScreen

--- src/main/TrackerScreen.js
+++ src/main/TrackerScreen.js
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader/root'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
@@ -13,11 +13,13 @@ import SettingsIcon from '@material-ui/icons/Settings'
 import { formatDate, getDayCounts } from '../lib/helpers'
 import Gauge from '../shared/Gauge'
 import GoalTrackerWidget from './GoalTrackerWidget'
+import { progressOnGoal } from '../reducers/todaysProgress'
 
 import './TrackerScreen.styl'
 
 const TrackerScreen = () => {
   const { goals, today, todaysProgress } = useSelector(selectState)
+  const dispatch = useDispatch()
 
   return (
     <Card className='goalTracker'>
@@ -30,6 +32,7 @@ const TrackerScreen = () => {
           <GoalTrackerWidget
             goal={goal}
             key={goal.id}
+            onProgress={markGoalProgression}
             progress={todaysProgress[goal.id] || 0}
           />
         ))}
@@ -44,6 +47,10 @@ const TrackerScreen = () => {
       </CardActions>
     </Card>
   )
+
+  function markGoalProgression({ id }) {
+    dispatch(progressOnGoal(id))
+  }
 
   function overallProgress() {
     const { totalProgress, totalTarget } = getDayCounts(todaysProgress, goals)
